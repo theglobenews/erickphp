dfe17bc85b40c1c6b1ac8ad3fbae79c3f0c9e1c6
theglobenews <ericksalustino@gmail.com>
2025-10-07 08:21:23 -0300

    subindo arquitetura mvc

---
+++ TO_DO_LIST-MVC/app/models/tarefa.php
@@ -0,0 +1,14 @@
+<?php 
+
+require_once __DIR__ . '/../config/database.php';
+
+class Tarefa{
+    private $conn;
+    public function __construct(){
+        $db = new Database();
+        $this->conn = $db->conectar();
+        
+    }
+}
+
+?>
