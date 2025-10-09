0d64c3a561a65b50ec67e5bca9842a4716394a9e
theglobenews <ericksalustino@gmail.com>
2025-10-09 11:40:53 -0300

    subindo arquivo de controller

---
+++ TO_DO_LIST-MVC/app/controllers/tarefaController.php
@@ -0,0 +1,30 @@
+<?php 
+
+require_once __DIR__ . '/../models/tarefa.php';
+
+class TarefasController {
+
+    private $tarefaModel;
+    public function __construct(){
+        $this->tarefaModel = new Tarefa();
+
+    }
+
+    # Criar:
+    public function criar(){
+        if(isset($POST['descricao']) && !empty(trim($_POST['descricao']))){
+            $this->tarefaModel->criar($_POST['descricao']);
+        }
+        header('Location; index.php');
+    }
+
+    ## Excluir:
+    public function excluir(){
+        if(isset($_GET['delete'])){
+            $this->tarefaModel->excluir($_GET);
+        }
+        header('Location: index.php');
+    }
+
+}
+?>
--- TO_DO_LIST-MVC/app/models/tarefa.php
+++ TO_DO_LIST-MVC/app/models/tarefa.php
@@ -9,6 +9,38 @@ class Tarefa{
         $this->conn = $db->conectar();
         
     }
-}
 
+    # Listar:
+    public function listar(){
+        $tarefas = [];
+        $sql = "SELECT * FROM tarefas ORDER BY data_criacao";
+        $resultado = $this->conn->query($sql);
+
+        if($resultado -> num_rows > 0){
+            while($row = $resultado -> fetch_assoc())
+                $taferas[]= $row;
+
+        }
+        return $tarefas;
+    }
+
+    ## Criar: 
+  
+    public function criar($descricao){
+        $descricao = $this->conn->real_escape_string($descricao);
+        $sql = "INSERT INTO tarefas (descricao) VALUES('$descricao')";
+        return $this->conn->query($sql);
+    }
+
+    ### Excluir:  
+
+    public function excluir($id){
+
+        $id = $this->conn->real_escape_string($id);
+        $sql = "DELETE FROM tarefas WHERE id = $id";
+        return $this->conn->query($sql);
+
+    }
+
+}
 ?>
