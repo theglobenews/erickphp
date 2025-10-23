995d0cb4e4b12a5f8aaf155f59a5fb99d56f66a5
theglobenews <ericksalustino@gmail.com>
2025-10-23 11:56:28 -0300

    subindo prova

--- TO_DO_LIST-MVC/app/controllers/tarefaController.php
+++ TO_DO_LIST-MVC/app/controllers/tarefaController.php
@@ -1,36 +1,44 @@
-<?php 
-
+<?php
 require_once __DIR__ . '/../models/tarefa.php';
-
 class TarefaController {
+    private $tarefaModel; 
 
-    private $tarefaModel;
     public function __construct(){
-        $this->tarefaModel = new Tarefa();
-
+        $this->tarefaModel = new Tarefa(); 
     }
 
-    # Criar:
+    public function index(){
+        $tarefas = $this->tarefaModel->listarAtivas(); 
+        include __DIR__ . '/../views/listar.php'; 
+    }
+
     public function criar(){
         if(isset($_POST['descricao']) && !empty(trim($_POST['descricao']))){
+            $this->tarefaModel->criar($_POST['descricao']); 
+        }
+        header('Location: index.php'); 
+    }
+
+    public function excluir(){
+        if(isset($_GET['delete'])){
+            $this->tarefaModel->excluir($_GET['delete']); 
+        }
+        header('Location: index.php'); 
+    }
+
+    public function editar() {
+        if (isset($_GET['id'])) {
+            $tarefas = $this->tarefaModel->buscarPorId($_GET['id']);
+            include __DIR__ . '/../views/editar.php';
+        }
+    }
+
+    public function atualizar() {
+        if (isset($_POST['id']) && isset($_POST['descricao'])) {
             $this->tarefaModel->criar($_POST['descricao']);
         }
         header('Location: index.php');
     }
-
-    # Excluir:
-    public function excluir(){
-        if(isset($_GET['delete'])){
-            $this->tarefaModel->excluir($_GET['delete']);
-        }
-        header('Location: index.php');
-    }
-
-    # Listar:
-    public function index(){
-        $tarefas = $this->tarefaModel->listar();
-        include __DIR__ . '/../views/listar.php';  
-    }
-
 }
+
 ?>
